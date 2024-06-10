fe74d81a742d56e9d79e0adfdc660a447851c692
MonagasVegas <35318244+MonagasVegas@users.noreply.github.com>
2024-06-10 17:12:26 -0300

    feat: add remove.

--- phrase-maker/src/components/Card/index.jsx
+++ phrase-maker/src/components/Card/index.jsx
@@ -3,11 +3,25 @@ import cita from "../../assets/images/cita.png";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { isEmpty } from "lodash";
 import SearchBar from "../Search/index";
+import TrashBin from "../../assets/svg/TrashBin.svg";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
 
 const Card = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [searchQuery, setSearchQuery] = useState("");
   const [filterPhrase, setFilterPhrase] = useState([]);
   const [auth] = useLocalStorage("@auth", []);
+  const [selected, setSelected] = useState({});
 
   const handleSearchQuery = (event) => {
     setSearchQuery(event.target.value);
@@ -28,16 +42,32 @@ const Card = () => {
     setFilterPhrase(newList);
   }, [auth, searchQuery]);
 
+  const handleSelected = (data) => () => {
+    setSelected(data);
+    onOpen();
+  };
+
+  const handleDelete = () => {
+    const idRemove = selected.id;
+    const updateData = filterPhrase.filter((data) => data.id !== idRemove);
+    setFilterPhrase(updateData);
+    onClose();
+  };
+
   return (
-    <>
+    <div className="">
       <div className=" py-4">
         <SearchBar onChange={handleSearchQuery} placeholder="Buscar frases" />
       </div>
 
-      <div className="py-10">
+      <div className="py-10   ">
         <div className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-5 px-[40px]">
           {filterPhrase.map((data) => (
-            <div key={data.id} className="  rounded-lg  bg-white">
+            <div
+              key={data.id}
+              onClick={handleSelected(data)}
+              className="rounded-lg  bg-white"
+            >
               <div className="px-7 py-4">
                 <img src={cita} width={40} />
               </div>
@@ -69,7 +99,28 @@ const Card = () => {
           </div>
         )}
       </div>
-    </>
+
+      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{`¿Deseas eliminar al autor ${selected.author}?`}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div className="flex flex-col justify-center items-center ">
+              <p className="font-semibold text-base leading-6 text-gray-800 mt-4">
+                Una vez eliminada, no podrás recuperar
+              </p>
+              <p className="font-semibold text-base leading-6 text-gray-800 py-2 ">
+                ¿Seguro desea eliminar esta frase ?
+              </p>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={handleDelete}>Eliminar</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </div>
   );
 };
 
--- phrase-maker/src/components/Home/index.jsx
+++ phrase-maker/src/components/Home/index.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       </div>
       <br />
       <br />
-      <div>
+      <div >
         <Card />
       </div>
     </div>
