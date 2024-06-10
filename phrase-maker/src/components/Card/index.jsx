994c6dbef257115e7f74ef0cf8d396b4429bbaa3
MonagasVegas <35318244+MonagasVegas@users.noreply.github.com>
2024-06-10 14:25:49 -0300

    feat: add search

--- phrase-maker/src/components/Card/index.jsx
+++ phrase-maker/src/components/Card/index.jsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import cita from "../../assets/images/cita.png";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { isEmpty } from "lodash";
+import SearchBar from "../Search/index";
 
 const Card = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filterPhrase, setFilterPhrase] = useState([]);
   const [auth] = useLocalStorage("@auth", []);
-  console.log("🐉 ~ Card ~ auth:", auth);
+
+  const handleSearchQuery = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setFilterPhrase(auth);
+    }
+    const newSearchQuery = searchQuery?.toLowerCase() || "";
+    // Garantizando la busqueda en minusculas
+    const newList = auth.filter(
+      (data) =>
+        data.author.toLowerCase().includes(newSearchQuery) ||
+        data.phrase.toLowerCase().includes(newSearchQuery)
+    );
+
+    setFilterPhrase(newList);
+  }, [auth, searchQuery]);
 
   return (
-    <div className="py-10">
-      <div className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-5 px-[40px]">
-        {auth.map((data) => (
-            <div 
-            key={data.author}
-            className="  rounded-lg  bg-white">
+    <>
+      <div className=" py-4">
+        <SearchBar onChange={handleSearchQuery} placeholder="Buscar frases" />
+      </div>
+
+      <div className="py-10">
+        <div className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-5 px-[40px]">
+          {filterPhrase.map((data) => (
+            <div key={data.id} className="  rounded-lg  bg-white">
               <div className="px-7 py-4">
                 <img src={cita} width={40} />
               </div>
@@ -22,9 +46,9 @@ const Card = () => {
                 style={{ aspectRatio: "16/9" }}
                 className="flex justify-center items-center  align-center px-5 "
               >
-                  <span className="text-black font-normal text-2xl  ">
-                    {data.phrase}
-                  </span>
+                <span className="text-black font-normal text-2xl  ">
+                  {data.phrase}
+                </span>
               </div>
 
               <div className="flex gap-3 px-3 py-3  items-end ">
@@ -34,17 +58,18 @@ const Card = () => {
                 </span>
               </div>
             </div>
-        ))}
-      </div>
-
-      {isEmpty(auth) && (
-        <div className="h-full w-full flex p-2 py-10 place-content-center">
-          <p className="text-center text-red-500">
-            Tu lista de frases está vacía.
-          </p>
+          ))}
         </div>
-      )}
-    </div>
+
+        {isEmpty(auth) && (
+          <div className="h-full w-full flex p-2 py-10 place-content-center">
+            <p className="text-center text-red-500">
+              Tu lista de frases está vacía.
+            </p>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
